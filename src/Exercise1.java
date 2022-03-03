9181042d8ea63689d96e8c09ac9c377c0dc36872
Édipo Nascimento <esnedipo@gmail.com>
2022-03-03 18:50:18 -0300

    solved ages comparasion

---
+++ src/Exercise1.java
@@ -0,0 +1,19 @@
+/*
+* Create a java program called CalculateDifferAge
+* create a variable to hold the first age
+* create a variable to hold de second age
+* create a variable to hold de difference between first and second age
+* display its content
+* */
+public class Exercise1 {
+
+    public static void main(String[] args) {
+
+        int firstAge = 34;
+        int secondAge = 23;
+        int result = firstAge - secondAge;
+
+        System.out.println("Result is: " + result);
+    }
+
+}
