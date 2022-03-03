7dfed392ec196d91629f1bcf9412fe604f8fe734
Édipo Nascimento <esnedipo@gmail.com>
2022-03-03 18:41:39 -0300

    solved 3 exercises

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+
+    public static void main(String[] args) {
+        int a = 3;
+        int b = 4;
+        int c = 7;
+
+        int result = ((a+b) / c); // 1
+        System.out.println("O resultado da operração é: " + result);
+
+    }
+
+}
---
+++ src/Main1.java
@@ -0,0 +1,14 @@
+public class Main1 {
+
+    public static void main(String[] args) {
+
+        int a = 3;
+        int b = 4;
+        int c = 7;
+
+        boolean result = (!((a > b) && (a < c))); // true
+        System.out.println("O resultado é: " + result);
+
+    }
+
+}
