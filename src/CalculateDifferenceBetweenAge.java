f358353bce5e4d1983b6bdc19df4b218f54a68e9
Édipo Nascimento <esnedipo@gmail.com>
2022-03-04 16:36:46 -0300

    Added calculate functionality

---
+++ src/CalculateDifferenceBetweenAge.java
@@ -0,0 +1,12 @@
+public class CalculateDifferenceBetweenAge {
+
+    public static void main(String[] args) {
+
+        int firstAge = 35;
+        int secondAge = 34;
+        int ageDifference = firstAge - secondAge;
+        System.out.println("Result age subtraction is: " + ageDifference);
+
+    }
+
+}
