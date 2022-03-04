0e3fe2a9e1eabe8bc1512c202bc3f53d4814a691
Édipo Nascimento <esnedipo@gmail.com>
2022-03-04 16:57:57 -0300

    percentagem exercise solved

---
+++ src/FeeCalculate.java
@@ -0,0 +1,11 @@
+
+public class FeeCalculate {
+    public static void main(String[] args) {
+        float salary = 15120;
+        float percentage = .07f;
+
+        float percentageOfSalary = salary * percentage;
+
+        System.out.println("Salary percentage is: " + percentageOfSalary);
+    }
+}
