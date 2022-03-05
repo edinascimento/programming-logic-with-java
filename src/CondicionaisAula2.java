9edef43db89838a652e37ec0d29033f5032f7732
Édipo Nascimento <esnedipo@gmail.com>
2022-03-05 15:08:08 -0300

    Added class fourteen Condition 2

---
+++ src/CondicionaisAula2.java
@@ -0,0 +1,18 @@
+/**
+ * Given an age as input
+ * if greater than 18 print "Adult"
+ * else print "Not an adult"
+ * */
+
+public class CondicionaisAula2 {
+    public static void main(String[] args) {
+
+        int age = 17;
+
+        if (age > 18) {
+            System.out.println("Adult");
+        } else {
+            System.out.println("Not an Adult!");
+        }
+    }
+}
